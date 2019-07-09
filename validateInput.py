804cf6a91a69fc17d4305846a9fdbace8e7d6146
Russell Loube <49661768+rjloube@users.noreply.github.com>
2019-07-08 20:44:43 -0500

    Create validateInput.py

---
+++ validateInput.py
@@ -0,0 +1,17 @@
+#! python3
+# validateInput.py - this program repeatedly asks users for
+# their age and password until they provide valid input.
+
+while True:
+    print('Enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for your age.')
+
+while True:
+    print('Select a new password (letters and numbers only):')
+    password = input()
+    if password.isalnum():
+        break
+    print('Passwords can only have letters and numbers.')
